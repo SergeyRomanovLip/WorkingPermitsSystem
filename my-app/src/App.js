bdfb2e3416cbf18934d207caf0bc040216545b64
Sergey Romanov <nashe.dobrov@gmail.com>
2020-04-06 13:58:41 +0300

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import MainMenu from "./MainMenu/MainMenu"
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Working permit system</h1>
+      <MainMenu />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/MainMenu/MainMenu.js
@@ -0,0 +1,14 @@
+import React from "react";
+const styles = {
+    ul:{
+        listStyle: 'none'
+    }
+}
+export default function MainMenu() {
+  return (
+    <ul style={styles.ul}>
+      <li>1</li>
+      <li>2</li>
+    </ul>
+  );
+}
